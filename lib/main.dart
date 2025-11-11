e59c67e1f153fb191d8773d28048cfb7ae69da03
munyokii <munyoki912@gmail.com>
2025-11-11 15:57:59 +0300

    Updated application with widgets and pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,39 @@
-import 'package:flutter/material.dart';
+// Importing necessary packages and files.
+//import 'package:firebase_core/firebase_core.dart'; // Firebase core package for initializing Firebase.
+import 'package:flutter/material.dart'; // Flutter's material design package for UI components.
+import 'package:mywallet/classes/homecontent.dart';
+//import 'package:my_pocket_wallet/firebase_options.dart'; // Firebase configuration options.
+import 'package:mywallet/screens/splashscreen.dart';
 
-void main() {
-  runApp(const MyApp());
+
+// The main function is the entry point of the Flutter application.
+void main() async {
+  // Initialize Firebase before running the app.
+  // await Firebase.initializeApp(
+  //   options: DefaultFirebaseOptions.currentPlatform, // Use the default Firebase options for the current platform.
+  // );
+  // Run the app by calling the MyPocketWallet widget.
+  runApp(const MyPocketWallet());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+// Root widget for the app.
+class MyPocketWallet extends StatelessWidget {
+  const MyPocketWallet({super.key}); // Constructor for the MyPocketWallet widget.
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    // The build method describes the part of the user interface represented by this widget.
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      debugShowCheckedModeBanner: false, // Hides the debug banner in the top-right corner.
+      title: 'My Pocket Wallet', // Title of the app.
+      theme: ThemeData(primarySwatch: Colors.blue), // Define the default theme for the app.
+      initialRoute: '/', // The initial route when the app starts.
+      routes: {
+        // Define the routes for the app.
+        '/': (context) => const Splashscreen(), // The root route, which shows the splash screen.
+        '/home': (context) => const Homecontent(), // The home route, which shows the home content.
+        // Add more routes as needed
+      },
     );
   }
 }
